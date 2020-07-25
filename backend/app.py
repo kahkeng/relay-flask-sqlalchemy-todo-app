63ea712c8f325cc4f7bfa30cb4acc598afcb377b
Kah Keng Tay <kahkeng@gmail.com>
2020-07-25 14:07:42 -0700

    backend: add code to support graphql on backend
    Incorporate the code snippets from https://docs.graphene-python.org/projects/sqlalchemy/en/latest/tutorial/

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,25 @@
 import time
 from flask import Flask
+from flask_graphql import GraphQLView
+
+from .models import db_session
+from .schema import schema, Department
 
 app = Flask(__name__)
+app.debug = True
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+app.add_url_rule(
+    '/graphql',
+    view_func=GraphQLView.as_view(
+        'graphql',
+        schema=schema,
+        graphiql=True # for having the GraphiQL interface
+    )
+)
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
+
+if __name__ == '__main__':
+    app.run()
---
+++ backend/seed.py
@@ -0,0 +1,16 @@
+from .models import engine, db_session, Base, Department, Employee
+Base.metadata.create_all(bind=engine)
+
+# Fill the tables with some data
+engineering = Department(name='Engineering')
+db_session.add(engineering)
+hr = Department(name='Human Resources')
+db_session.add(hr)
+
+peter = Employee(name='Peter', department=engineering)
+db_session.add(peter)
+roy = Employee(name='Roy', department=engineering)
+db_session.add(roy)
+tracy = Employee(name='Tracy', department=hr)
+db_session.add(tracy)
+db_session.commit()
